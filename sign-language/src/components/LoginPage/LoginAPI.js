3715dee949713b08f809238bd3492efd481860b9
Edlix <56119942+Edlix@users.noreply.github.com>
2021-09-21 15:36:21 +0200

    Login api

--- sign-language/src/components/LoginPage/LoginAPI.js
+++ sign-language/src/components/LoginPage/LoginAPI.js
@@ -1,11 +1,18 @@
 export const LoginAPI = {
 
-    // login(credentials) {
-    //     return fetch ('https://vue-questionaire.herokuapp.com/translations'), {
-    //         po
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     }
-    // }
+    login(credentials) {
+        return fetch ('https://vue-questionaire.herokuapp.com/translations', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then( async response => {
+            if (!response.ok) {
+                const {error = 'An unknown error occurd'} = await response.json()
+                throw new Error(error)
+            }
+        })
+    }
 }
