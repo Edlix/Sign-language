99240dcc042de6c7e39ea07e98a3c598caf904d7
Edlix <56119942+Edlix@users.noreply.github.com>
2021-09-21 15:15:08 +0200

    Redux setup

---
+++ sign-language/src/components/LoginPage/LoginAPI.js
@@ -0,0 +1,11 @@
+export const LoginAPI = {
+
+    // login(credentials) {
+    //     return fetch ('https://vue-questionaire.herokuapp.com/translations'), {
+    //         po
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     }
+    // }
+}
