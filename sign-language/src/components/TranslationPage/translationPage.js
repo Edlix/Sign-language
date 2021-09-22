49e8d61fbd0c30a1e55a0f9fd031137742239760
Martin V. Stenberg <martin.v.stenberg@gmail.com>
2021-09-22 08:03:51 +0200

    Comitting,

--- sign-language/src/components/TranslationPage/translationPage.js
+++ sign-language/src/components/TranslationPage/translationPage.js
@@ -1,18 +1,29 @@
 import AppContainer from "../../hoc/AppContainer";
 
 function TranslationPage() {
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+    }
     return (
         <AppContainer>
             <main className="TranslationPage">
-                <form className="mt-3">
+                <form className="mt-3" onSubmit={handleSubmit}>
                     <h2>Translatooor for Sign Language</h2>
                     <div>
-                        <label> LOL</label>
+                        <label htmlFor="input" className="form-label">Text to be translated</label>
+                        <input id="input" type="text" placeholder="Enter text" className="form-control" />
+                    </div>
+
+                    <h2>Signe Language</h2>
+                    <div>
+
                     </div>
                 </form>
             </main>
         </AppContainer>
     )
+
 }
 
 
